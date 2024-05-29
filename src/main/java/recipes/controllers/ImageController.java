dbe851b45083f6165d0d5e8f0c90bbb5cb618ba4
ProstoRyder <denisyk.deonis@gmail.com>
2024-05-29 16:05:41 +0300

    add Photo

--- src/main/java/recipes/controllers/ImageController.java
+++ src/main/java/recipes/controllers/ImageController.java
@@ -20,7 +20,7 @@ public class ImageController {
     private ResponseEntity<?> getImageById(@PathVariable Long id){
         Image image = imageRepository.findById(id).orElse(null);
         return ResponseEntity.ok()
-                .header("foleName", image.getOriginalFileName())
+                .header("fileName", image.getOriginalFileName())
                 .contentType(MediaType.valueOf(image.getContentType()))
                 .contentLength(image.getSize())
                 .body(new InputStreamResource(new ByteArrayInputStream(image.getBytes())));
