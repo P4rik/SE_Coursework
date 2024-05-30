bfc36af6ebf22d9151f273f9613af746328ac905
ProstoRyder <denisyk.deonis@gmail.com>
2024-05-30 20:57:14 +0300

    add сorrelation

---
+++ src/main/java/recipes/controllers/UserController.java
@@ -0,0 +1,37 @@
+package recipes.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import recipes.module.User;
+import recipes.services.UserService;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private  final UserService userService;
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+    @PostMapping("/registration")
+    public String createUser(User user, Model modul) {
+        if (!userService.createUser(user)){
+            modul.addAttribute("errorMessage","користувач з email: " + user.getEmail() + "вже існує");
+            return "registration";
+        }
+        return "redirect:/login";
+    }
+
+
+    @GetMapping("/hello")
+    public String securityUrl() {
+        return "hello";
+    }
+}
---
+++ src/main/java/recipes/services/CustomUserDetailsService.java
@@ -0,0 +1,18 @@
+package recipes.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import recipes.repositories.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
+    }
+}
