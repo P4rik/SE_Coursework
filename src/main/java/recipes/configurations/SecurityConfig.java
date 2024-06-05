db81b52ddfcd51083b83037b1cf5ea0b5f539a59
Parik <danilparow@gmail.com>
2024-06-05 22:20:12 +0300

    update Back-End

--- src/main/java/recipes/configurations/SecurityConfig.java
+++ src/main/java/recipes/configurations/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,14 +15,15 @@ import recipes.services.CustomUserDetailsService;
 @EnableWebSecurity
 @Configuration
 @AllArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private final CustomUserDetailsService userDetailsService;
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/", "/registration").permitAll()
-                .requestMatchers("/recipe/**", "/image/**")
+                .requestMatchers("/", "/registration","/images/**","/recipe/**", "/static/**", "/user/**").permitAll()
+                .requestMatchers( "/create/**")
                 .hasAnyAuthority("ROLE_ADMIN","ROLE_USER")
                 .anyRequest().authenticated()
         )
